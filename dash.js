ef949e8571c0a4d5ce26346d750bb70d7b149a52
teebu <junkbox0@gmail.com>
2016-07-16 23:14:35 -0700

    buttons plural

--- dash.js
+++ dash.js
@@ -1,5 +1,5 @@
 var dash_button = require('node-dash-button');
-var dash = dash_button(["44:65:0d:b3:fb:e6"]); // get the mac address of dash button
+var dash = dash_button(["44:65:0d:b3:fb:e6"]); // get the mac address of dash buttons
 
 dash.on("detected", function (dash_id){
   // do stuff here
