5f653916d3a0289e03fba8dd86be261daee70182
teebu <junkbox0@gmail.com>
2016-07-17 11:46:39 -0700

    auto detect ip based on network card passed

--- dash.js
+++ dash.js
@@ -2,6 +2,6 @@ var dash_button = require('node-dash-button');
 var dash = dash_button(["44:65:0d:b3:fb:e6"]); // get the mac address of dash buttons
 
 dash.on("detected", function (dash_id){
-  // do stuff here
+  // todo: do stuff here
   console.log("omg found", dash_id);
 });
