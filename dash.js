02a9a28a2d39a42fb832bed1d215fa63f56e6b0f
teebu <junkbox0@gmail.com>
2016-07-17 16:35:28 -0700

    added twitter

--- dash.js
+++ dash.js
@@ -1,7 +1,18 @@
-var open = require('open');
-var request = require('request');
+var open = require('open'),
+    request = require('request'),
+    dash_button = require('node-dash-button-winpcap'),
+    Twitter = require('twitter'),
+    debug = false,
+    enableITTT = false
 
-var dash_button = require('node-dash-button-winpcap');
+//twitter stuff
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+
 var dash = dash_button(["44:65:0d:b3:fb:e6"]); // get the mac address of dash buttons
 
 dash.on("detected", function (dash_id){
@@ -15,8 +26,12 @@ dash.on("detected", function (dash_id){
   //doRequest('http://127.0.0.1:8888/pluginrpc/Obyekt%20666%20C2.0/do/pp') // pause/play
   doRequest('http://127.0.0.1:8888/pluginrpc/Obyekt%20666%20C2.0/do/ne') // next song
 
-});
+  // post tweet
+  var tweet = 'Hello, it is ' + new Date()
+  if (enableITTT) tweet += ' #button01 activated!' // enable ITTT to do stuff, it is triggers on #button01 hashtag
+  //postTweet(tweet, function(err,res){ console.log('Tweet:', res.text) })
 
+});
 
 function doRequest(requestUrl, method, options, callback) {
   options = options || {}
@@ -51,4 +66,13 @@ function doRequest(requestUrl, method, options, callback) {
       callback(error, response, body)
     }
   })
+}
+
+function postTweet(text, callback){
+  client.post('statuses/update', {status: text},  function(error, tweet, response){
+    if(error) console.log(error)
+    if (debug) console.log(tweet.text);  // Tweet body.
+    //console.log(response);  // Raw response object.
+    return callback(error,tweet)
+  });
 }
